319056132d734c60ee63783fa1534867155c1afe
Tallha07 <tallhamuddassar@yahoo.com>
2024-07-30 20:07:36 -0500

    Update 02-reverseString.js

--- mod1/week-01/02-tue/problems/02-reverseString.js
+++ mod1/week-01/02-tue/problems/02-reverseString.js
@@ -6,9 +6,16 @@ Write a function called reverseString. It will take in a string and reverse it. 
 HINT: Try using the typeof functionality (you may have to look it up on MDN and play around with it)
 
 */
-
-
-
+function reverseString(str){
+  if(str.length === 0 || typeof str !== "string"){
+    return "Did not pass in a valid string"
+  }
+  let res = "";
+  for(let i = str.length - 1; i >= 0; i--){
+    let letter = str[1];
+    res += letter;
+}
+console.log(reverseString("hello"));
 
 /* DO NOT MODIFY ANYTHING BELOW THIS LINE */
 
